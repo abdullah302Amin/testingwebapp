674002e77d48afd1050de9ecfed5b5cdc58dc6a8
abdullah302Amin <muhammad.abdullah@contour-software.com>
2025-02-07 01:55:11 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 # Define the route for the homepage
 @app.route('/')
 def hello():
-    return "Hello, welcome to the homepage!"
+    # Return HTML with a line break between the messages
+    return "Hello, welcome to the homepage!<br>Good to see you!"
 
 # Run the app
 if __name__ == '__main__':
