caadf92acc877175ebfc121b05d83d24cff2f44e
abdullah302Amin <muhammad.abdullah@contour-software.com>
2025-02-10 16:06:19 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,19 @@ app = Flask(__name__)
 # Define the route for the homepage
 @app.route('/')
 def hello():
-    # Return HTML with a line break between the messages
-    return "Hello, welcome to the homepage!<br>Good to see you!!<br>you are on production slot"
+    # Return HTML with a link to the second page
+    return """
+        Hello, welcome to the homepage!<br>
+        Good to see you!!<br>
+        You are on the production slot.<br>
+        <a href="/second_page">Go to the second page</a>
+    """
+
+# Define the route for the second page
+@app.route('/second_page')
+def second_page():
+    # Return a message for the second page
+    return "Hello to the second page!"
 
 # Run the app
 if __name__ == '__main__':
