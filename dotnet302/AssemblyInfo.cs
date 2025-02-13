0fa377fe8cbfedf660400d809d0944ca4863b363
abdullah302Amin <muhammad.abdullah@contour-software.com>
2025-02-13 19:30:26 +0500

    Create AssemblyInfo.cs

---
+++ dotnet302/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("dotnet302")]
+[assembly: AssemblyDescription("Simple ASP.NET 4.8 Web App")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("dotnet302")]
+[assembly: AssemblyCopyright("Copyright © 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("3b0c6765-e2a6-4e42-bfc4-03cd9a974d4a")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
