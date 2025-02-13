0b84fffad1ebbd2c9975a617fe5e8491e6d4ad17
abdullah302Amin <muhammad.abdullah@contour-software.com>
2025-02-13 18:22:22 +0500

    Create Global.asax.cs

---
+++ SimpleWebApp/Global.asax.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Web;
+
+namespace SimpleWebApp
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+        }
+    }
+}
