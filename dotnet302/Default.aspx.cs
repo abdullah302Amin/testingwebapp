781c243c543d826df554bd1caa7b7230d38e75f2
abdullah302Amin <muhammad.abdullah@contour-software.com>
2025-02-13 19:29:43 +0500

    Create Default.aspx.cs

---
+++ dotnet302/Default.aspx.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Web.UI;
+
+namespace dotnet302
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Logic on page load (optional)
+        }
+    }
+}
