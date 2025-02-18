2c99cd80717b36d9f5a59efe2836b52607526aa8
abdullah302Amin <muhammad.abdullah@contour-software.com>
2025-02-18 21:24:53 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,61 @@
-Microsoft Visual Studio Solution File, Format Version 12.00
-# Visual Studio Version 16
-VisualStudioVersion = 16.0.32105.134
-MinimumVisualStudioVersion = 10.0.40219.1
-Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "SimpleWebApp", "SimpleWebApp\SimpleWebApp.csproj", "{GUID}"
-EndProject
-Global
-    GlobalSection(SolutionConfigurationPlatforms) = preSolution
-        Debug|Any CPU = Debug|Any CPU
-        Release|Any CPU = Release|Any CPU
-    EndGlobalSection
-    GlobalSection(ProjectConfigurationPlatforms) = postSolution
-        {GUID}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
-        {GUID}.Debug|Any CPU.Build.0 = Debug|Any CPU
-        {GUID}.Release|Any CPU.ActiveCfg = Release|Any CPU
-        {GUID}.Release|Any CPU.Build.0 = Release|Any CPU
-    EndGlobalSection
-    GlobalSection(SolutionProperties) = preSolution
-        HideSolutionNode = FALSE
-    EndGlobalSection
-EndGlobal
+from flask import Flask, render_template, request, jsonify
+import requests
+import uuid
+
+app = Flask(__name__)
+
+# Add your key and endpoint
+key = "BF8WQCfzUar73JNOqUj5y0dcHU5lW1RGFycJtSeMgPYb366Tf69gJQQJ99BBAC1i4TkXJ3w3AAAbACOGgMDQ"
+endpoint = "https://api.cognitive.microsofttranslator.com"
+
+# Location, also known as region.
+location = "centralus"
+
+path = '/translate'
+constructed_url = endpoint + path
+
+params = {
+    'api-version': '3.0',
+    'from': 'en',
+    'to': ['fr', 'zu', 'ur']
+}
+
+headers = {
+    'Ocp-Apim-Subscription-Key': key,
+    'Ocp-Apim-Subscription-Region': location,
+    'Content-type': 'application/json',
+    'X-ClientTraceId': str(uuid.uuid4()),
+}
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        # Get user input from the form
+        user_input = request.form.get("text")
+
+        # Use the user input in the body
+        body = [{
+            'text': user_input
+        }]
+
+        # Send the request to the API
+        response = requests.post(constructed_url, params=params, headers=headers, json=body)
+        translation_response = response.json()
+
+        # Extract translations from the response
+        translations = []
+        for translation in translation_response[0]['translations']:
+            translations.append({
+                'language': translation['to'],
+                'translated_text': translation['text']
+            })
+
+        # Render the page with the translations
+        return render_template("index.html", translations=translations, original_text=user_input)
+
+    # If it's a GET request, just render the form
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
